882c769aea6246c220c432fb71645919f9843c4f
Dario Marquez <darioemanuelmarquez@gmail.com>
2020-05-18 22:06:37 -0300

    fdfd

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 const logger = require("./logger"); //esto definido como una constante no va a permitir que se cambie dentro de la app.
-const path = require('path');
-const os  = require('os');
-const fs = require('fs');
+const fs = require("fs");
 
-let pathObj = path.parse(__filename);
-logger.log(pathObj);
+//Tareas
 
-let freeMem = os.freemem();
-let totalMem = os.totalmem();
+//1-2 Create a method to get all the files inside given path and show
+function getAllFilesInsidePath(path) {
+    let files = fs.readdirSync(path);
+    return files;
+}
 
-logger.log('free mem: ',freeMem);
-logger.log("Total mem: ",totalMem);
+let files = getAllFilesInsidePath("./");
+console.log("\nActividad 1 y 2: ")
+logger.log("Archivos: ", files);
 
-let rootDir = fs.readdirSync('./');
-logger.log("root: ", rootDir);
 
-fs.readdir('./', (err, files) => {
-    (err) ? console.log(err) : console.log(files);
-})
+//3- Create a method to sort and print all files.
+function reverseSort(filesList){
+    let files = filesList.reverse();
+    logger.log("Archivos ordenados de forma inversa: ", files);
+}
+console.log("\nActividad 3: ")
+reverseSort(files);
 
-console.log(rootDir);
+//4 - Create a method to get the amount of files starting with one letter
+function getAmountFilesStartingWith(letter, path){
+    let files = getAllFilesInsidePath(path);
+    let count = 0;
+    for (let index = 0; index < files.length; index++) {
+        const element = files[index];
+        if(element.startsWith(letter)){
+            count ++;
+        }
+        
+    }
+
+    return count
+}
+
+let cantidad = getAmountFilesStartingWith("a", "./");
+console.log("\nActividad 4: ");
+logger.log("Cantidad: ", cantidad);
+
+
+
+
+
+
